e93b018240b5b8e20b2a49054a722fafbe43adcf
OseSem <91069296+OseSem@users.noreply.github.com>
2024-06-16 16:40:17 +0200

    Changes to example files

--- examples/get_map_ss.py
+++ examples/get_map_ss.py
@@ -12,12 +12,12 @@ async def main() -> None:
     """Run main."""
     logging.basicConfig(level=logging.DEBUG)
     logging.getLogger("rustmaps").setLevel(logging.DEBUG)
+
     client = Client("f808bde9d91244b788ea145eff54497f")
 
     map_seed = input("Enter a map seed: ")
     map_size = input("Enter a map size: ")
     found_map = await client.get_map_ss(int(map_seed), int(map_size))
-    # found_map = await client.create_map()
     print(found_map)
     await client.http.close()
 
