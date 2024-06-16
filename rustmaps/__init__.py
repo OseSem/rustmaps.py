c01f6f8b5de5702db0c6f1b2c135faa63f8a94ad
OseSem <91069296+OseSem@users.noreply.github.com>
2024-06-16 16:35:32 +0200

    Add type-checking for __init__.py

--- rustmaps/__init__.py
+++ rustmaps/__init__.py
@@ -3,14 +3,15 @@
 """An API Wrapper for rustmaps.com written in Python."""
 
 import logging
+from typing import Final
 
 from .client import *
 from .errors import *
 from .http import *
 
-__version__ = "0.1.0"
-__author__ = "OseSem"
-__license__ = "MIT"
-__title__ = "rustmaps.py"
+__version__: Final[str] = "0.1.0"
+__author__: Final[str] = "OseSem"
+__license__: Final[str] = "MIT"
+__title__: Final[str] = "rustmaps.py"
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
